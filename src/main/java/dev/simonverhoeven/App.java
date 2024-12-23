e729c6bf312e8acdc1790a7a216c24b697c46096
Simon Verhoeven <verhoeven.simon@gmail.com>
2024-12-23 05:28:02 +0100

    feature: update samples

--- src/main/java/dev/simonverhoeven/App.java
+++ src/main/java/dev/simonverhoeven/App.java
@@ -1,10 +1,25 @@
 package dev.simonverhoeven;
 
+import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
 
+import java.util.Arrays;
+
 public class App {
-    // Array of nullable texts
-    private String @Nullable[] arrayOfNullableTexts = new String[5];
+    // Array of nullable Strings
+    private final String[] arrayOfNullableTexts;
     // Nullable array
-    private @Nullable String[] nullableArray = null;
+    private String @Nullable [] nullableArray = null;
+    // Nullable array
+    private @Nullable String @Nullable [] nullableArrayOfNullableStrings = null;
+
+    public App() {
+        arrayOfNullableTexts = new String[]{null, "", null, null, null};
+        nullableArray = new String[]{null, "", null, null, null};
+    }
+
+    public String[] getArrayOfNullableTexts() {
+        System.out.println(Arrays.toString(nullableArray));
+        return arrayOfNullableTexts;
+    }
 }
